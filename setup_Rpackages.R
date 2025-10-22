f57e681f58fa387ffcd2a3e717afe700964247da
brry <berryboessenkool@hotmail.com>
2025-10-22 13:42:56 +0200

    include usethis (needed if setup_cran_mirror has not been run)

--- setup_Rpackages.R
+++ setup_Rpackages.R
@@ -2,4 +2,4 @@
 # in case you're prompted to seltect a CRAN mirror, see setup_cran_mirror first
 
 if(!requireNamespace("pacman", quietly=TRUE)) install.packages("pacman")
-pacman::p_load("reticulate", "rmarkdown", "rdwd", "remotes")
+pacman::p_load("reticulate", "rmarkdown", "rdwd", "remotes", "usethis")
