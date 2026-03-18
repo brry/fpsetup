15816c8c3541859f21670a63b54edf05c5aecdec
brry <berryboessenkool@hotmail.com>
2026-03-18 23:18:54 +0100

    reference to next step

--- 3d_check_python_setup.py
+++ 3d_check_python_setup.py
@@ -1,9 +1,10 @@
-# This script tests whether the needed python modules are available
+# This script tests whether the needed python modules are available.
+
 print("Seeing if modules can be loaded ...")
 import numpy
 import pandas
 import matplotlib
-print("It worked! You're good to go :)")
+print("It worked! You're good continue in step 4 :)")
 
 # If this fails, go back to Step 3c or 
 # press `CTRL` + `SHIFT` + `P`, search "Python: Select Interpreter", 
