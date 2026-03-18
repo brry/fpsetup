b7c8a4d6a732623855a7e29ab2f5c69303a3863f
brry <berryboessenkool@hotmail.com>
2026-03-18 23:04:21 +0100

    instructions expanded

--- 2d_install_Rpackages.R
+++ 2d_install_Rpackages.R
@@ -1,5 +1,6 @@
-# this installs some R packages we will need in the course
-# in case you're prompted to select a CRAN mirror, see 4c_set_cran_mirror.R first.
+# This script installs (if not already done) some R packages we will need in the course.
+# Run it in Rstudio with CTRL + SHIFT + S or clicking "Source" in the topright.
+# In case you're prompted to select a CRAN mirror, see 4c_set_cran_mirror.R first.
 
 if(!requireNamespace("pacman", quietly=TRUE)) install.packages("pacman")
 pacman::p_load("reticulate", "rmarkdown", "rdwd", "remotes", "usethis")
