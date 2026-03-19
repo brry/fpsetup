b34028b212f230dcb6e4421a1b96d0140ce5d3c7
brry <berryboessenkool@hotmail.com>
2026-03-19 12:39:03 +0100

    explicit for VScode

--- 3d_check_python_setup.py
+++ 3d_check_python_setup.py
@@ -8,7 +8,7 @@ import pandas
 import matplotlib
 print("It worked! You're good continue in step 4 :)")
 
-# If this fails, go back to Step 3c or 
+# If this fails, go back to Step 3c or in VScode,
 # press `CTRL` + `SHIFT` + `P`, search "Python: Select Interpreter", 
 # set to the path from `3b_install_python.R`
 
