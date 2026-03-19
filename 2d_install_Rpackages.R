3b63a0c28e0c2c2f73dad9fdf7fd459a1b95a91f
brry <berryboessenkool@hotmail.com>
2026-03-19 12:01:25 +0100

    RStudio capitalization unified

--- 2d_install_Rpackages.R
+++ 2d_install_Rpackages.R
@@ -1,5 +1,5 @@
 # This script installs (if not already done) some R packages we will need in the course.
-# Run it in Rstudio with CTRL + SHIFT + S or clicking "Source" in the topright.
+# Run it in RStudio with CTRL + SHIFT + S or clicking "Source" in the topright.
 # In case you're prompted to select a CRAN mirror, see 4c_set_cran_mirror.R first.
 
 if(!requireNamespace("pacman", quietly=TRUE)) install.packages("pacman")
