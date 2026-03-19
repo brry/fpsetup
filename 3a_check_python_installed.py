b2d46714a98aa61326f8d06f169c0246894a8881
brry <berryboessenkool@hotmail.com>
2026-03-19 12:00:26 +0100

    missing sys import added

--- 3a_check_python_installed.py
+++ 3a_check_python_installed.py
@@ -1,8 +1,10 @@
 # This script displays where Python is installed.
-# - in Rstudio, click "Source Script" in the topright of the script panel.
+# - in RStudio, click "Source Script" in the topright of the script panel.
 # - in VScode, run it by clicking the play triangle in the topright.
 # If Python is not installed yet, do not blindly follow the IDE suggestions (if given),
 # but go to step 3b of the guide instead.
+
+import sys
 
 print(f"Python Executable:    {sys.executable}")
 print(f"Python Version:       {sys.version.split()[0]}")
